f60b13549c6669efe2ca732f6d96605cab7038aa
Cindy Colucy <ckcolucy@gmail.com>
2017-12-18 12:10:13 -0500

    "challenge fail"

--- src/com/colucy/Main.java
+++ src/com/colucy/Main.java
@@ -21,9 +21,30 @@ public class Main {
         System.out.println("Your final score was " + highScore);
     }
 
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
 
-        if(gameOver){
+    int position = calculateHighScorePosition(1500);
+
+
+    public static void displayHighScorePosition (String playerName, int position){
+        System.out.println(playerName + "managed to get into position " + position + "on the high score table");
+    }
+
+    public static int calculateHighScorePosition (int score){
+        if (score > 1000) {
+            return 1;
+        }
+        else if (score > 500 && score < 1000) {
+            return 2;
+        }
+        else if (score > 100 && score < 500) {
+            return 3;
+        }
+        else return 4;
+
+    }
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+
+        if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 2000;
             return finalScore;
