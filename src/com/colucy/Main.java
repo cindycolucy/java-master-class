a7f2bd4314e44ceeabe3ce5508b134cca0fc4994
Cindy Colucy <ckcolucy@gmail.com>
2017-12-19 11:54:34 -0500

    "Main"

--- src/com/colucy/Main.java
+++ src/com/colucy/Main.java
@@ -4,54 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
-
-        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("Your final score was " + highScore);
-
-
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-
-        highScore = calculateScore(gameOver, score, levelCompleted, bonus);
-        System.out.println("Your final score was " + highScore);
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.model = "Carrera";
     }
-
-
-    int position = calculateHighScorePosition(1500);
-
-
-
-    public static void displayHighScorePosition (String playerName, int position){
-        System.out.println(playerName + "managed to get into position " + position + "on the high score table");
-    }
-
-    public static int calculateHighScorePosition (int score){
-        if (score > 1000) {
-            return 1;
-        }
-        else if (score > 500 && score < 1000) {
-            return 2;
-        }
-        else if (score > 100 && score < 500) {
-            return 3;
-        }
-        else return 4;
-
-    }
-    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
-
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            finalScore += 2000;
-            return finalScore;
-        }
-        return -1;
-    }
-
 
 }
